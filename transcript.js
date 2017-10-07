ac5c02d753424e2265c48798041cf05e7070a242
Stéfano Stypulkowski Zanata <made.in.stefano@gmail.com>
2017-10-07 02:17:23 -0300

    Added support to proper scape rails interpolation

--- transcript.js
+++ transcript.js
@@ -2,7 +2,7 @@ const fs = require( 'fs' );
 const yaml = require( 'js-yaml' );
 const trans = require( './lib/transformation' );
 
-const [ format, baseFile, targetFile, output ] = process.argv.slice( 2 );
+const [ format, baseFile, targetFile, output, ...extras ] = process.argv.slice( 2 );
 
 const content = {};
 
@@ -19,5 +19,26 @@ const newContent = trans.run( content.base, content.target );
 if ( format === 'json' ) {
   fs.writeFileSync( output, JSON.stringify( newContent, null, 2 ) + '\n' );
 } else if ( format === 'yaml' ) {
-  fs.writeFileSync( output, yaml.safeDump( newContent, { indent: 2, lineWidth: -1, noCompatMode: true } ) );
+  let dump = yaml.safeDump( newContent, {
+    indent: 2,
+    lineWidth: -1,
+    noCompatMode: true
+  } );
+
+  if ( extras.includes('--rails') ) {
+    dump = dump.split('\n').map( line => {
+      let [ k, ...v ] = line.split(": ");
+      v = v.join(': ');
+
+      if ( v.includes('%{') && v.startsWith("'") ) {
+        v = v.replace(/"/g, '\\"');
+        v = v.replace(/^'|'$/g, '"');
+        v = v.replace(/'+/g, "'");
+        return `${k}: ${v}`;
+      }
+      return line;
+    }).join('\n')
+  }
+
+  fs.writeFileSync( output, dump );
 }
